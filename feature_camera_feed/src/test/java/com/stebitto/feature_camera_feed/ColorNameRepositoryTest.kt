885c65ddc5b5f3fc6d883b6be2b71e369c3a1665
Stefano Bitto <stefano.bitto.dev@gmail.com>
2024-12-04 09:03:46 +0100

    ColorNameRepository test

---
+++ feature_camera_feed/src/test/java/com/stebitto/feature_camera_feed/ColorNameRepositoryTest.kt
@@ -0,0 +1,51 @@
+package com.stebitto.feature_camera_feed
+
+import com.stebitto.common.MainDispatcherRule
+import com.stebitto.feature_camera_feed.data.ColorNameRemoteSource
+import com.stebitto.feature_camera_feed.data.ColorNameRepositoryImpl
+import com.stebitto.feature_camera_feed.data.retrofit.ColorRemoteModel
+import com.stebitto.feature_camera_feed.data.retrofit.NameRemoteModel
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+class ColorNameRepositoryTest {
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @Mock
+    private lateinit var colorNameRemoteSource: ColorNameRemoteSource
+
+    private lateinit var colorNameRepository: ColorNameRepositoryImpl
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+        colorNameRepository = ColorNameRepositoryImpl(colorNameRemoteSource)
+    }
+
+    @Test
+    fun `getColorName should return the correct color name`() = runTest {
+        val colorName = "Test Color"
+        val remoteModel = ColorRemoteModel(NameRemoteModel(colorName, "#000000"))
+        Mockito.`when`(colorNameRemoteSource.getColorName(123, 456, 789)).thenReturn(remoteModel)
+        val result = colorNameRepository.getColorName(123.0, 456.0, 789.0)
+        assert(result.isSuccess)
+        assertEquals(result.getOrNull(), colorName)
+    }
+
+    @Test
+    fun `getColorName should return an error when the remote source throws an exception`() = runTest {
+        val exception = RuntimeException("Test exception")
+        Mockito.`when`(colorNameRemoteSource.getColorName(123, 456, 789)).thenThrow(exception)
+        val result = colorNameRepository.getColorName(123.0, 456.0, 789.0)
+        assert(result.isFailure)
+        assertEquals(result.exceptionOrNull(), exception)
+    }
+}
